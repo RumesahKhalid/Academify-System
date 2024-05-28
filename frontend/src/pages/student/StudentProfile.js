d826b586d8b34b43588f356b2baf477b84d6819d
Rumesah <https://github.com/RumesahKhalid/Travel.git>
2024-05-29 02:52:59 +0500

    initial

---
+++ frontend/src/pages/student/StudentProfile.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Card, CardContent, Typography, Grid, Box, Avatar, Container, Paper } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+const StudentProfile = () => {
+  const { currentUser, response, error } = useSelector((state) => state.user);
+
+  if (response) { console.log(response) }
+  else if (error) { console.log(error) }
+
+  const sclassName = currentUser.sclassName
+  const studentSchool = currentUser.school
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <StyledPaper elevation={3}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center" >
+              <img
+  alt="Student Avatar"
+  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3URjWpcZfPfzAHxrU_Xms2GzfUJmvWXGjuw&s"
+  width={150}
+  height={150}
+/>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center" >
+                <Typography variant="h5" component="h2" textAlign="center" >
+                  {currentUser.name}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center">
+                <Typography variant="subtitle1" component="p" textAlign="center">
+                  Student Roll No: {currentUser.rollNum}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center">
+                <Typography variant="subtitle1" component="p" textAlign="center">
+                  Class: {sclassName.sclassName}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="center">
+                <Typography variant="subtitle1" component="p" textAlign="center">
+                  School: {studentSchool.schoolName}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </StyledPaper>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Personal Information
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Date of Birth:</strong> January 1, 2000
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Gender:</strong> Female
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Email:</strong> unknown@gmail.com
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Phone:</strong> (+92) 456-789098
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Address:</strong> 123 Main Street, City, Country
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="subtitle1" component="p">
+                  <strong>Emergency Contact:</strong> (+92) 654-321090
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
+  )
+}
+
+export default StudentProfile
+
+const StyledPaper = styled(Paper)`
+  padding: 20px;
+  margin-bottom: 20px;
+`;
